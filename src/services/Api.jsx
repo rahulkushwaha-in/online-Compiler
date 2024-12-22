1051a0aa0514fc5f9bfea6d39a25ae9a796ef4c6
rahulkushwaha-in <awesomerahul.vl@gmail.com>
2024-12-23 02:55:50 +0530

    version 3 complete nodejs endpoint

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -2,7 +2,7 @@
 const apiUrl = import.meta.env.VITE_API_URL;
 export async function executeCode(code) {
   try {
-    const response = await fetch(`${apiUrl}/nodejs`, {
+    const response = await fetch("https://vfupw381da.execute-api.ap-south-1.amazonaws.com/default/executeNode", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
